4a495268ef5e2673d1fc50aa54415e0879a7654f
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-17 13:55:16 +0530

    responsiveness added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="flex items-center">
       <div
-        className="relative heroElem w-full h-full sm:h-[80rem] pt-24 pb-40 m-auto flex flex-col-reverse sm:flex-row justify-between text-center items-center z-1"
+        className="relative heroElem w-full h-[60rem] sm:h-full pt-24 pb-40 m-auto flex flex-col-reverse sm:flex-row justify-between text-center items-center z-1"
         style={{ maxWidth: "1200px" }}
       >
-        <div className="w-[60%]">
+        <div className="w-11/12 sm:w-[60%]">
           <p className="text-xl mb-5 sm:block">Hey, I&apos;m Yash.</p>
           <h1 className="heroTitle inline-block max-w-2xl lg:max-w-3xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
             I enjoy <span className="heroShiny1 text-fun">building</span> and{" "}
@@ -18,20 +18,10 @@ export default function Home() {
               style={{ animationDelay: "0.1s" }}
               src="/static/doodles/hero/html.svg"
             />
-            {/* <img
-            className="sqD hidden sm:block bottom-[-340px] left-[-180px]"
-            style={{ animationDelay: "0.4s" }}
-            src="/static/doodles/hero/coder.svg"
-          /> */}
             <img
-              className="sqD hidden sm:block left-[100px] lg:left-[160px] bottom-[-150px]"
+              className="sqD right-[0px] lg:right-[60px] bottom-[-107px]"
               style={{ animationDelay: "0.5s" }}
               src="/static/doodles/hero/js.svg"
-            />
-            <img
-              className="sqD right-[-60px] sm:right-0 bottom-[-180px] lg:[5%]"
-              style={{ animationDelay: "0.7s" }}
-              src="/static/doodles/hero/paintbrush.svg"
             />
             <img
               className="sqD squiggle-hero-pop1 hidden sm:block sm:top-[-130px] sm:left-[15%] lg:top-[-130px] lg:left-[120px]"
@@ -44,7 +34,7 @@ export default function Home() {
             />
           </h1>
         </div>
-        <div className="w-[40%]">
+        <div className="w-10/12 sm:w-[40%]">
           <HeroBanner />
         </div>
       </div>
