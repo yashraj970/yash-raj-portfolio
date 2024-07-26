457c42aea2745bf0ba730bbb398271417f662406
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-26 16:06:33 +0530

    Light mode color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
           <div className="w-11/12 sm:w-[60%]">
             <p className="text-xl mb-5 sm:block">Hey, I&apos;m Yash.</p>
             <h1 className="heroTitle inline-block max-w-2xl lg:max-w-3xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
-              I enjoy <span className="heroShiny1 text-fun">building</span> and{" "}
-              <span className="heroShiny2 text-fun">designing</span> for the
+              I enjoy <span className="text-fun">building</span> and{" "}
+              <span className="text-fun">designing</span> for the
               web.
               <img
                 className="sqD squiggle-hero-html w-16 top-[-90px] right-[5%] sm:top-[-90px] sm:right-[170px]"
