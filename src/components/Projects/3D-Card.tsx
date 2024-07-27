7da897479b326b320508abec44b3b11fb8422d33
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-27 20:26:44 +0530

    title,   description,   src, made as props

--- src/components/Projects/3D-Card.tsx
+++ src/components/Projects/3D-Card.tsx
@@ -5,7 +5,15 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 import Link from "next/link";
 
-export function ThreeDCardDemo() {
+export function ThreeDCardDemo({
+  title,
+  description,
+  src,
+}: {
+  title: string;
+  description: string;
+  src: string;
+}) {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[25rem] h-auto rounded-xl p-6 border">
@@ -13,18 +21,18 @@ export function ThreeDCardDemo() {
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          {description}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={src}
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,9 +5,27 @@ const Projects = () => {
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        <ThreeDCardDemo />
-        <ThreeDCardDemo />
-        <ThreeDCardDemo />
+        <ThreeDCardDemo
+          title={"Make things float in air"}
+          src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          description={
+            "Hover over this card to unleash the power of CSS perspective"
+          }
+        />
+        <ThreeDCardDemo
+          title={"Make things float in air"}
+          src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          description={
+            "Hover over this card to unleash the power of CSS perspective"
+          }
+        />
+        <ThreeDCardDemo
+          title={"Make things float in air"}
+          src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          description={
+            "Hover over this card to unleash the power of CSS perspective"
+          }
+        />
       </div>
     </div>
   );
