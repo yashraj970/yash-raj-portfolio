c5fc5c804d6043c3a10cb16b8cb10938ef705541
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-30 23:10:55 +0530

    responsiveness added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroBanner from "@/components/HeroBanner/HeroBanner";
 import HeroText from "@/components/HeroBanner/HeroText";
 import { MacbookScrollDemo } from "@/components/MacbookScroll/MacbookScrollDemo";
+import { HireMeModal } from "@/components/Navbar/HireMe";
 import Projects from "@/components/Projects/Projects";
 import { Button } from "@/components/ui/moving-border";
 import Link from "next/link";
@@ -9,16 +10,19 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className="relative heroElem w-full min-h-[30rem] h-[37rem] sm:h-full pt-2 m-auto flex flex-col-reverse sm:flex-row justify-start text-center items-center ">
+        <div className="relative heroElem w-full min-h-[33rem] h-[40rem] sm:h-full pt-2 m-auto flex flex-col-reverse sm:flex-row justify-start text-center items-center">
           <div className="w-11/12 sm:w-[60%]">
             <HeroText />
-            <div className="mt-5">
+            <div className="sm:hidden mt-3">
+              <HireMeModal />
+            </div>
+            <div className="mt-3 sm:mt-6">
               <Link href={"/about"}>
                 <Button>Tell Me More</Button>
               </Link>
             </div>
           </div>
-          <div className="w-10/12 sm:w-[30%]">
+          <div className="w-10/12 sm:w-[40%] lg:w-[30%]">
             <HeroBanner />
           </div>
         </div>
--- src/components/HeroBanner/HeroText.tsx
+++ src/components/HeroBanner/HeroText.tsx
@@ -7,7 +7,7 @@ const HeroText = () => {
   return (
     <div className="flex flex-col justify-center items-center px-0 sm:px-2">
       <p className="text-2xl my-5">Hey, I&apos;m Yash.</p>
-      <div className="text-5xl md:text-6xl font-bold mx-auto text-neutral-800 dark:text-neutral-300">
+      <div className="text-4xl sm:text-5xl lg:text-6xl font-bold mx-auto text-neutral-800 dark:text-neutral-300">
         I enjoy <span className="text-fun">building</span>
         <br />
         <FlipWords words={words} /> web applications.
