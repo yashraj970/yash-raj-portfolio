082ed64517ef3938b29aeb64ea44f01a26c645da
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-08-09 16:03:29 +0530

    Footer changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import LanguageIcon from "@mui/icons-material/Language";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
+import { AnimatedTooltip } from "../ui/animated-tooltip";
 
 const socialLinks = [
   {
@@ -30,99 +31,45 @@ const socialLinks = [
   },
 ];
 
+const data = [
+  {
+    id: 1,
+    name: "Portfolio",
+    designation: "Yash Raj",
+    image: "/images/footerIcons/portfolio.jpg",
+    url: "https://yash-raj-portfolio.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Github",
+    designation: "yashraj970",
+    image: "/images/footerIcons/github.png",
+    url: "https://github.com/yashraj970",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    // designation: "Data Scientist",
+    image: "/images/footerIcons/linekdin.jpeg",
+    url: "https://www.linkedin.com/in/yash-raj-a9037b242/",
+  },
+  {
+    id: 4,
+    name: "Email",
+    designation: "itsyashraj970@gmail.com",
+    image: "/images/footerIcons/gmail.jpeg",
+    url: "mailto:itsyashraj970@gmail.com",
+  },
+];
+
 function Footer() {
   return (
     <footer className="flex flex-col w-full px-5 py-10 border-t border-fun-pink-darker z-5 bg-bg">
-      {/* <div className="w-full max-w-4xl m-auto grid grid-cols-2 sm:grid-cols-3 justify-between items-start">
-        {footer.columns.map((item, index) => {
-          return (
-            <div key={index} className="text-left mb-5 sm:mb-0">
-              <h4 className="uppercase text-fun-gray text-sm font-bold">
-                {item.title}
-              </h4>
-              <div>
-                {item.links.map((item, index) => {
-                  return (
-                    <div key={index} className="my-4">
-                      {item.leavesWebsite ? (
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          className="items-center flex"
-                        >
-                          {item.icon && (
-                            <span className="pr-2 -mb-1">
-                              <Image
-                                src={item.icon}
-                                width={20}
-                                height={20}
-                                alt="icon"
-                              />
-                            </span>
-                          )}
-                          {item.name}
-                        </a>
-                      ) : (
-                        <Link href={item.link}>{item.name}</Link>
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-        <div className="text-center col-span-2 sm:col-auto sm:text-left pt-8 sm:mt-0 sm:pt-0 text-fun-gray border-t border-fun-pink-dark sm:border-0">
-          <h4 className="uppercase text-fun-gray text-sm font-bold">
-            Support My Work
-          </h4>
-          <div className="space-y-2 mt-4 w-full flex items-center sm:items-start flex-col">
-            {footer.support.buymeacoffee !== "" && (
-              <div>
-                <a
-                  href={`https://buymeacoffee.com/${footer.support.buymeacoffee}`}
-                  target="_blank"
-                >
-                  <img
-                    src="/static/misc/buy-me-a-coffee.svg"
-                    className="h-12 mr-2 hover:opacity-80 opacity-100 transition-opacity"
-                  />
-                </a>
-              </div>
-            )}
-            {footer.support.paypal !== "" && (
-              <div>
-                <a
-                  href={`https://paypal.me/${footer.support.paypal}`}
-                  target="_blank"
-                >
-                  <img
-                    src="/static/misc/paypal.svg"
-                    className="h-12 mr-2 hover:opacity-80 opacity-100 transition-opacity"
-                  />
-                </a>
-              </div>
-            )}
-            <p className="text-fun-gray text-xs pt-1">
-              {footer.support.message}
-            </p>
-          </div>
-        </div>
-      </div> */}
       <div className="flex flex-col items-center">
-        <p className="text-3xl mb-5">Follow me</p>
-        <ul className="flex gap-4 sm:gap-8">
-          {socialLinks.map((link) => (
-            <a
-              href={link.url}
-              target="__blank"
-              key={link.id}
-              className="bg-gray-200 dark:bg-gray-700 cursor-pointer rounded-full shadow-sm p-3 sm:p-3.5 duration-300"
-            >
-              <i className="text-primary">{link.icon}</i>
-            </a>
-          ))}
-        </ul>
+        <p className="text-3xl mb-5">Socials</p>
+        <div className="flex flex-row gap-1 items-center justify-center mb-2 w-full">
+          <AnimatedTooltip items={data} />
+        </div>
       </div>
       <div className="max-w-4xl w-full m-auto mt-8 pt-8 sm:mt-4 sm:pt-4 text-center text-fun-gray border-t border-fun-pink-dark">
         <p className="flex flex-col items-center justify-center ">
