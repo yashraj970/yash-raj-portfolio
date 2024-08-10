357c2758ec2dd3d24373b5f4ba03478f526afd07
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-08-10 12:24:09 +0530

    Footer data imported from global data

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,66 +1,7 @@
-import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { footer } from "@/data/globalData";
-import LanguageIcon from "@mui/icons-material/Language";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
 import { AnimatedTooltip } from "../ui/animated-tooltip";
-
-const socialLinks = [
-  {
-    id: 1,
-    icon: <LanguageIcon />,
-    url: "https://yash-raj-portfolio.vercel.app/",
-  },
-  {
-    id: 2,
-    icon: <GitHubIcon />,
-    url: "https://github.com/yashraj970",
-  },
-  {
-    id: 4,
-    icon: <LinkedInIcon />,
-    url: "https://www.linkedin.com/in/yash-raj-a9037b242/",
-  },
-  {
-    id: 5,
-    icon: <EmailIcon />,
-    url: "mailto:itsyashraj970@gmail.com",
-  },
-];
-
-const data = [
-  {
-    id: 1,
-    name: "Portfolio",
-    designation: "Yash Raj",
-    image: "/images/footerIcons/portfolio.jpg",
-    url: "https://yash-raj-portfolio.vercel.app/",
-  },
-  {
-    id: 2,
-    name: "Github",
-    designation: "yashraj970",
-    image: "/images/footerIcons/github.png",
-    url: "https://github.com/yashraj970",
-  },
-  {
-    id: 3,
-    name: "LinkedIn",
-    // designation: "Data Scientist",
-    image: "/images/footerIcons/linekdin.jpeg",
-    url: "https://www.linkedin.com/in/yash-raj-a9037b242/",
-  },
-  {
-    id: 4,
-    name: "Email",
-    designation: "itsyashraj970@gmail.com",
-    image: "/images/footerIcons/gmail.jpeg",
-    url: "mailto:itsyashraj970@gmail.com",
-  },
-];
 
 function Footer() {
   return (
@@ -68,7 +9,7 @@ function Footer() {
       <div className="flex flex-col items-center">
         <p className="text-3xl mb-5">Socials</p>
         <div className="flex flex-row gap-1 items-center justify-center mb-2 w-full">
-          <AnimatedTooltip items={data} />
+          <AnimatedTooltip items={footer} />
         </div>
       </div>
       <div className="max-w-4xl w-full m-auto mt-8 pt-8 sm:mt-4 sm:pt-4 text-center text-fun-gray border-t border-fun-pink-dark">
