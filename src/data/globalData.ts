33d8046757e8ce5b6df1214549a9bade88077e5e
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-08-11 23:20:28 +0530

    Project details added for voice calling

--- src/data/globalData.ts
+++ src/data/globalData.ts
@@ -13,45 +13,6 @@ export const routes = [
   },
 ];
 
-//Footer
-
-interface Footer {
-  id: number;
-  name: string;
-  designation?: string;
-  image: string;
-  url: string;
-}
-export const footer: Footer[] = [
-  {
-    id: 1,
-    name: "Portfolio",
-    designation: "Yash Raj",
-    image: "/images/footerIcons/portfolio.jpg",
-    url: "https://yash-raj-portfolio.vercel.app/",
-  },
-  {
-    id: 2,
-    name: "Github",
-    designation: "yashraj970",
-    image: "/images/footerIcons/github.png",
-    url: "https://github.com/yashraj970",
-  },
-  {
-    id: 3,
-    name: "LinkedIn",
-    image: "/images/footerIcons/linekdin.jpeg",
-    url: "https://www.linkedin.com/in/yash-raj-a9037b242/",
-  },
-  {
-    id: 4,
-    name: "Email",
-    designation: "itsyashraj970@gmail.com",
-    image: "/images/footerIcons/gmail.jpeg",
-    url: "mailto:itsyashraj970@gmail.com",
-  },
-];
-
 // Projects
 export interface content {
   image: string;
@@ -102,7 +63,26 @@ export const projects: Project[] = [
       "AI-driven voice calling platform that leverages OpenAI and Twilio to integrate voice chat calling.",
     liveLink: "",
     github: "",
-    content: [],
+    content: [
+      {
+        image: "/images/projects/phonenumbers.png",
+        detailTitle: "Phone Numbers Page: Manage Your Phone Numbers",
+        detailDesc:
+          "Import and manage your phone numbers from Twilio or Vonage. Easily add, edit, and delete phone numbers by providing the required credentials, including number, token, and SID. Keep your calling resources organized and accessible in one centralized location.",
+      },
+      {
+        image: "/images/projects/contactlist.png",
+        detailTitle: "Contact Lists: Upload and Organize Your Contact Lists",
+        detailDesc:
+          "Seamlessly upload and manage contact lists for your calling campaigns. Assign contacts to specific AI assistants, view, edit, and delete lists as needed. This page provides a streamlined process for organizing your calling targets to ensure efficient outreach.",
+      },
+      {
+        image: "/images/projects/callLogs.png",
+        detailTitle: "Call Logs: Access and Review Call Logs",
+        detailDesc:
+          "Listen to recordings and review the history of all calls made through the system. Search by date, view detailed transcripts, and keep track of your communication efforts with an intuitive, user-friendly interface designed for easy navigation and access.",
+      },
+    ],
   },
   {
     title: "Fire Alarm Management System",
@@ -112,5 +92,43 @@ export const projects: Project[] = [
     liveLink: "",
     github: "",
     content: [],
+  },
+];
+
+//Footer
+interface Footer {
+  id: number;
+  name: string;
+  designation?: string;
+  image: string;
+  url: string;
+}
+export const footer: Footer[] = [
+  {
+    id: 1,
+    name: "Portfolio",
+    designation: "Yash Raj",
+    image: "/images/footerIcons/portfolio.jpg",
+    url: "https://yash-raj-portfolio.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Github",
+    designation: "yashraj970",
+    image: "/images/footerIcons/github.png",
+    url: "https://github.com/yashraj970",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    image: "/images/footerIcons/linekdin.jpeg",
+    url: "https://www.linkedin.com/in/yash-raj-a9037b242/",
+  },
+  {
+    id: 4,
+    name: "Email",
+    designation: "itsyashraj970@gmail.com",
+    image: "/images/footerIcons/gmail.jpeg",
+    url: "mailto:itsyashraj970@gmail.com",
   },
 ];
