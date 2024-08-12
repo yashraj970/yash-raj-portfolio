effe0de53ed60efa5f111c1865bab12a9d699f2f
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-08-12 17:01:46 +0530

    close modal on cancel button

--- src/components/Navbar/HireMe.tsx
+++ src/components/Navbar/HireMe.tsx
@@ -90,9 +90,6 @@ export function HireMeModal() {
             </div>
           </ModalContent>
           <ModalFooter className="gap-4">
-            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
-              Cancel
-            </button>
             <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
               Send
             </button>
