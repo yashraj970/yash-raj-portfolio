2132c5345aa045a1514917f65072659789e02c18
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-08 15:57:28 +0530

    Created first api in next js

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  return NextResponse.json({
+    success: true,
+    data: {
+      name: "test api",
+    },
+  });
+}
