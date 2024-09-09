5aa0d0f85d6ac7fdac05a9ee084a38b735310507
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-09 17:23:54 +0530

    Update route.ts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,12 +2,13 @@ import { mailOptions, transporter } from "@/config/nodemailer";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  const data = await req.json();
   try {
     transporter.sendMail({
       ...mailOptions,
       subject: "Email From Portfolio",
       text: "Test String",
-      html: "<h1>Mail Title</h1>",
+      html: `<h1>Mail From - ${data.name}</h1>`,
     });
     return NextResponse.json({ message: "Mail Sent Succesfully" });
   } catch (error) {
