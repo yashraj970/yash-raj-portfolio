35ae323a007f339e461e6acdc6485ac68a1854c6
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-09 16:40:33 +0530

    Update route.ts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,15 +1,6 @@
 import { mailOptions, transporter } from "@/config/nodemailer";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  return NextResponse.json({
-    success: true,
-    data: {
-      name: "test api",
-    },
-  });
-}
-
 export async function POST(req: Request) {
   try {
     transporter.sendMail({
