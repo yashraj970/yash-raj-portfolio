f2fcecff82fcd3bdd0c7add47295b954e6290f91
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-10 15:28:20 +0530

    toast called after mail sent

--- src/components/Navbar/HireMe.tsx
+++ src/components/Navbar/HireMe.tsx
@@ -14,6 +14,8 @@ import { Input } from "../ui/input";
 import { cn } from "@/lib/utils";
 import axios from "axios";
 import { CircularProgress } from "@mui/material";
+import { notifySuccess } from "../../utils/toast";
+
 const images = [
   "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -46,6 +48,7 @@ export function HireMeModal() {
       .post("/api/email", data)
       .then((res) => {
         setLoading(false);
+        notifySuccess(res?.data?.message || "Details Sent");
         setData(initialState);
       })
       .catch((err) => {
