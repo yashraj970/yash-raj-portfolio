8b1384efd51f0dedee5ce0a949f12f4692f303b8
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-23 17:01:58 +0530

    Update container-scroll-animation.tsx

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -36,11 +36,11 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[50rem] md:h-[70rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[50rem] md:h-[70rem] flex justify-center relative p-2"
       ref={containerRef}
     >
       <div
-        className="py-10 md:py-40 w-full relative"
+        className="py-4 md:py-10 w-full relative"
         style={{
           perspective: "1000px",
         }}
