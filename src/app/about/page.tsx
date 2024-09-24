073306c3fcfb3ff017a01a27cffc602629fdc8b9
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-09-24 14:24:39 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,9 +18,9 @@ const page = () => {
           titleComponent={
             <>
               <h1 className="text-4xl font-semibold text-black dark:text-white">
-                Unleash the power of <br />
+                Always Evolving <br />
                 <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                  Scroll Animations
+                  
                 </span>
               </h1>
             </>
